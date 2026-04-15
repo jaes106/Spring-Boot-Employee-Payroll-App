e25f34c0ddf239c8bbbed9b7ff31b69e8aeffa66
Jason Abraham Rajan <jasonrajan20@gmail.com>
2026-04-15 17:32:20 +0530

    [Jason] feat : initialize employee payroll app with basic endpoint

---
+++ src/main/java/com/bridgelabz/employeepayrollapp/controller/EmployeePayrollController.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.employeepayrollapp.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/employeepayrollservice")
+public class EmployeePayrollController {
+
+    @GetMapping("/")
+    public String getMessage() {
+        return "Employee Payroll App is Running";
+    }
+}
