df43026d2f7d1a2fb46648840b87451f09b6adec
Jason Abraham Rajan <jasonrajan20@gmail.com>
2026-04-16 11:58:19 +0530

    [Jason] feat : add controller http methods

--- src/main/java/com/bridgelabz/employeepayrollapp/controller/EmployeePayrollController.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/controller/EmployeePayrollController.java
@@ -6,8 +6,34 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/employeepayrollservice")
 public class EmployeePayrollController {
 
+
     @GetMapping("/")
     public String getMessage() {
         return "Employee Payroll App is Running";
+    }
+
+
+    @GetMapping("/get/{id}")
+    public String getEmployee(@PathVariable int id) {
+        return "Get Employee with ID: " + id;
+    }
+
+
+    @PostMapping("/create")
+    public String createEmployee(@RequestBody String body) {
+        return "Created Employee: " + body;
+    }
+
+
+    @PutMapping("/update/{id}")
+    public String updateEmployee(@PathVariable int id,
+                                 @RequestBody String body) {
+        return "Updated Employee " + id + " with data: " + body;
+    }
+
+
+    @DeleteMapping("/delete/{id}")
+    public String deleteEmployee(@PathVariable int id) {
+        return "Deleted Employee with ID: " + id;
     }
 }
