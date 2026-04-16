cbef5e686b6fe5ed40af379454e969f8611bd1e3
Jason Abraham Rajan <jasonrajan20@gmail.com>
2026-04-16 14:14:21 +0530

    [Jason] feat : add logging using lombok @Slf4j

--- src/main/java/com/bridgelabz/employeepayrollapp/service/EmployeePayrollService.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/service/EmployeePayrollService.java
@@ -2,26 +2,32 @@ package com.bridgelabz.employeepayrollapp.service;
 
 import com.bridgelabz.employeepayrollapp.dto.EmployeePayrollDTO;
 import com.bridgelabz.employeepayrollapp.model.EmployeePayrollData;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Service
 public class EmployeePayrollService {
 
     private List<EmployeePayrollData> employeeList = new ArrayList<>();
     private int idCounter = 1;
 
-    // CREATE + STORE IN LIST
     public EmployeePayrollData create(EmployeePayrollDTO dto) {
+        log.info("Creating employee: {}", dto.getName());
+
         EmployeePayrollData emp = new EmployeePayrollData(idCounter++, dto);
         employeeList.add(emp);
+
         return emp;
     }
 
-    // OPTIONAL (to verify storage)
     public List<EmployeePayrollData> getAll() {
+        log.info("Fetching all employees");
         return employeeList;
     }
 }
