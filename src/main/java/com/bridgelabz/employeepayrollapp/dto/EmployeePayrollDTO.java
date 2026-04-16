80a4b6da5585b6e12c64f0e8df8aa924b3e5ffa9
Jason Abraham Rajan <jasonrajan20@gmail.com>
2026-04-16 14:00:51 +0530

    [Jason] feat : integrate lombok to reduce boilerplate in dto

--- src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
@@ -1,25 +1,9 @@
 package com.bridgelabz.employeepayrollapp.dto;
 
-public class EmployeePayrollDTO {
+import lombok.Data;
 
+@Data
+public class EmployeePayrollDTO {
     private String name;
     private double salary;
-
-    public EmployeePayrollDTO() {}
-
-    public String getName() {
-        return name;
-    }
-
-    public double getSalary() {
-        return salary;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setSalary(double salary) {
-        this.salary = salary;
-    }
 }
